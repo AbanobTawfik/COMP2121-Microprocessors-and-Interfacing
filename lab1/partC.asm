32b3a845e630d72eba7b944399a70b56c8a44d78
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-03-16 20:51:43 +1100

    Update partC.asm

--- lab1/partC.asm
+++ lab1/partC.asm
@@ -3,32 +3,33 @@
 .equ lowertouppercase=32					; (will be -32 as lowercase have higher ascii values
 .def letter = r16							; a register for pointing to a letter expecting 0x74 as r16 first
 
+.dseg
+arrayFinal:
+	.byte 20
+
+.cseg
 myword: 
-	.db "thiswordshudbeup1_2",0			    ;making sure to pad so no misallignment Cx
-	.cseg 
+	.db "thiswordshudbeup1_2",0				;making sure to pad so no misallignment Cx
+   ldi ZL, low(myword<<1)   
+   ldi ZH, high(myword<<1)					;loading into the X - pointer the string above me ^^^^
 
-start:
-
-   ldi r30, low(myword<<1)   
-   ldi r31, high(myword<<1)					;loading into the X - pointer the string above me ^^^^
-
-   ldi r28, low(0x200)	
-   ldi r29, high(0x200)						;loading the pointer of Y into SRAM 0x200 is start of sram
+   ldi YL, low(arrayFinal)	
+   ldi YH, high(arrayFinal)						;loading the pointer of Y into SRAM 0x200 is start of sram
 
 moveIntoMemory:
-   lpm letter, z+							;loading our letters into program memori dont be dumb like me use the z pointer
-   st y+, letter							;now storing that letter into the SRAM
+   lpm letter, Z+							;loading our letters into program memori dont be dumb like me use the z pointer
+   st Y+, letter							;now storing that letter into the SRAM
    cpi letter, ending
    brne moveIntoMemory						;check if we copied the entire word into sram if we havent then go back to copy function
 
 
    ;now we want our beloved y pointer to the start of our SRAM which will have our word
-   ldi r28, low(0x200)   
-   ldi r29, high(0x200)						;loading the pointer of Y into SRAM
+   ldi YL, low(arrayFinal)   
+   ldi YH, high(arrayFinal)						;pointing Y to start of arrayFinal (contains stored array of ints)
 
 
 makeUpper:
-	ld letter, y							;now that our string is loaded into sram, convert char by char
+	ld letter, Y							;now that our string is loaded into sram, convert char by char
 	cpi letter, ending						;check if we are at the end of the string
 	breq halt								;if its the end end the whole thing done
 	cpi letter, 97							;want to check if its a lowercase letter now a = 97 z = 123
@@ -40,7 +41,7 @@ makeUpper:
 	subi letter, lowertouppercase
 
 nextcharr:
-	st y+, letter
+	st Y+, letter
 	rjmp makeUpper
 
    halt:
