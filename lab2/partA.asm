a261dd62a70a5195f73fec33997d86cf814019fb
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-03-22 22:22:14 +1100

    fixed up memory issues

--- lab2/partA.asm
+++ lab2/partA.asm
@@ -5,58 +5,54 @@
 ; Created: 11/03/2018 8:34:38 PM
 ; Author : Abs Tawfik
 ;
-.def stringlength = r17
-.def singlecharacter = r16
+.dseg
+retword:
+	.byte 20
 
+
+.cseg
 word: .db "thisshudreverse",0
 
-	;making our X pointer 
+	;loading our word into Z reg from program memory 
     ldi ZL, low(word<<1)
 	ldi ZH, high(word<<1)
-	;setting up the stack
-	ldi r18, low(RAMEND)
-	out SPL, r18
-	ldi r18, high(RAMEND)
-	out SPL, r18
 
-	ldi YL, low(0x200)
-	ldi YH, low(0x200)
-	ldi stringlength, 0
+	;setting up the stack
+	ldi XL, low(RAMEND-20)		;20 bytes to store 20 characters in stack
+	out SPL, XL					;adjust the stack pointer to point to the top of stack
+	ldi XH, high(RAMEND-20)
+	out SPH, XH
+
+	ldi YL, low(retword)		;load the address of return array into Y pointer
+	ldi YH, high(retword)
+	ldi r17, 0			;counter for our word length
 
 puttheword:
 	;loop to store the word into memory
-	lpm singlecharacter, Z+
-	cpi singlecharacter, 0
+	lpm r16, Z+
+	cpi r16, 0
 	breq reverseString
 
-	;else you want to push onto the stack
-	push singlecharacter
-	;increment our string length
-	inc stringlength  
+	;else you want to push onto the stack the character from program memory
+	push r16
+	;increment our string length to keep track of our string length
+	inc r17  
 	rjmp puttheword
 
 reverseString:
 	
-	;pop our letter off stack (this is how we reverse Boi
-	pop singlecharacter
-	dec stringlength
-	st Y+, singlecharacter
+	;pop our letter off stack stack is LIFO so will reverse the input
+	pop r16
+	dec r17
+	st Y+, r16	;store the word in reverse order into our return array (Y pointer points to return array)
 
-	cpi stringlength, 0
+	cpi r17, 0
 	breq addnullcharacter
 	rjmp reverseString
 
 addnullcharacter:
-	ldi singlecharacter, 0
-	st Y+, singlecharacter
-	ldi YL, low(0x200)
-	ldi YH, low(0x200)
-
-printstring:
-	ld singlecharacter, Y+
-	cpi singlecharacter, 0
-	breq halt
-	rjmp printstring
+	ldi r16, 0
+	st Y+, r16
 
 halt:
 	rjmp halt
