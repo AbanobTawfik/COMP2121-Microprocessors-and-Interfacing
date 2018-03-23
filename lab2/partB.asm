49bb4583e34172a9f78af5eadcd6b2693089b190
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-03-24 09:53:40 +1100

    fixed lower and uppercase Z

--- lab2/partB.asm
+++ lab2/partB.asm
@@ -1,18 +1,12 @@
 .include "m2560def.inc"
-;defining upper case limits
-.equ A = 65
-.equ Z = 90
-;defining lower case limits
-.equ a = 97
-.equ z = 122
 
 .cseg
 rjmp start
 
 validstring:
-.db "abcdABCD", 0
+.db "abcdABCDzZ",0
 invalidstring:
-.db "74(*&Q#$^}{:?<>", 0
+.db "74(*&Q#$^}{:?<>",0
 
 start:
 ;initialised stack here
@@ -33,61 +27,75 @@ mov r21, r16
 ; r21 should be 0
 
 
-push ZL
-push ZH
-push r16
-push r17
-push r18
+
 
 halt:
 rjmp halt
 
 checkalpha:
 ;(if((r17>'A' && r17 <'Z') || (r17 > 'a' && r17 < 'z')
-
+push ZL
+push ZH
+push r17
+push r18
+push r0   ;return address
 checkCharacters:
 lpm r17, Z+
 cpi r17, 0	;check if character is end of string if not keep looping
-beq endOfFunctionSuccess
+breq endOfFunctionSuccess
 ;load A into r18
-ldi r18, A
-; if >A go to second check 
+ldi r18, 'A'
+; if  < A that means its not even in range of character 
 cp r17, r18
-brlt checkupperz
-ldi r18, a
-cp r17, r18
-brlt checklowerz
-rjmp checkCharacters
+;if >= A we want to check if <=Z
+brsh checkupperz
+;if <A do this
+rjmp endOfFunctionFail
 
 checkupperz:
-ldi r18, Z
+ldi r18, 'Z'
+;compare our Z with our letter
 cp r18, r17
-brlt checkCharacters
+;if our letter >Z we want to check if its an uppercase letter then
+brlt checkiflowercaseinstead
+;otherwise its >=A <=Z its a uppercase character check next
+rjmp checkCharacters
+
+checkiflowercaseinstead:
+;check if its >=a
+ldi r18, 'a'
+cp r17, r18
+;if its >=a check if its <=z
+brsh checklowerz
+;otherwise its not a letter if its not >=a but >=z
 rjmp endOfFunctionFail
 
 checklowerz:
-ldi r18, z
-cp r18, r17
-brlt checkCharacters
-rjmp endOfFunctionFail
+;check if the character is <=z 
+ldi r18, 'z'
+cp r18,r17
+;if its >z then its a symbol of a sort or special character  
+brlt endOfFunctionFail
+;otherwise jump to next character to checl
+rjmp checkCharacters
 
 endOfFunctionSuccess:
 ;load success into r16 -> 1
-;return
+;return values from stack esp return address r0 to ret to next call
 ldi r16, 1
+pop r0
 pop r18
 pop r17
-pop r16
 pop ZH
 pop ZL
 ret
 endOfFunctionFail:
 ;load fail into r16 -> 0
-;return
+;return values from stack esp return address r0 to ret to next call
 ldi r16, 0
+pop r0
 pop r18
 pop r17
-pop r16
 pop ZH
 pop ZL
 ret
