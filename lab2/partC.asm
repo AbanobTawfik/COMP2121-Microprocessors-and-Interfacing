822a8f8ae377e6fdfc3e3923c763e49fb2739020
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-03-23 14:32:08 +1100

    Update partC.asm

--- lab2/partC.asm
+++ lab2/partC.asm
@@ -1,1 +1,30 @@
+.include "m2560def.inc"
 
+.cseg
+myString1: .db "*A%%BCD$$EFG$$$$"
+myString2: .db "$%()@#*%@(#*%"    ;using for isSYMBOL
+
+ldi r16, low(RAMEND)
+out SPL, r16
+ldi r16, high(RAMEND)		;!!! Insert stack initialization code here !!!
+out SPH, r16             
+
+;load the string into Z
+ldi ZL, low(myString1 << 1)
+ldi ZH, high(myString1 << 1)
+;load Function into Y
+
+ld YL, low(isSymbol)
+ld YH, high(isSymbol)
+
+mov r20, r16    
+call checkString
+
+ldi ZL, low(myString2 << 1)
+ldi ZH, high(myString2 << 1)
+;load Function into Y
+
+ld YL, low(isSymbol)
+ld YH, high(isSymbol)
+
+call checkString
