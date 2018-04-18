d3b1d61eefdfb12a89473bc9f6bed2a8490db9e6
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-04-19 02:18:05 +1000

    left shift now wrapped

--- lab3/partB.asm
+++ lab3/partB.asm
@@ -52,7 +52,7 @@ RESET:
 	out SPH, temp
 	ldi temp, low(RAMEND)
 	out SPL, temp
-	;now we want to set our port C as output (all pins)
+	;now we want to set our port C as output (all leds as output)
 	ser temp
 	out DDRC, temp
 	rjmp main
@@ -76,12 +76,15 @@ Timer0OVF:
 	ldi temp, high(7812)
 	cpc r25, temp
 	brne NotSecond			;if the register pair are not 7812, a second hasnt passed so we jump to NotSecond which will increase counter by 1
+	
 	;if this condition has not been met then we will update our pattern by shifting it by 1 to left?
 	;shifting our 16 bit pattern to the left has two steps
 	;first as quoted from an AVRFREAKS post forum on shifting 16 bit numbers, we want to perform a lsl on the upper regster
-	;then we want to do a ROL - rotate left through carry on the lower register, this will take the carry and put it 
-	;in the bit 7 of the lower 8 bits, this will be wrapped itself this way as the carry is in bit 7 (last bit of the lower bits)
-	;here we are multiplying number by 2 to left shift by the way, 
+	;then we want to do a ROL - rotate left through carry on the lower register
+	;now we check if the carry flag has been set from our first lsl call
+	;if it has that means we now need to adjust our pattern (create a wrapper)
+	;if it has we want to manually load that carry flag into the last bit of the upper part of the string
+	;to do that we manually load and store bit using bld and bst
 	lds r24, patternState
 	lsl r24 
 	;store updated pattern which was logically left shifted by 1
@@ -90,21 +93,52 @@ Timer0OVF:
 	lds r24, patternState + 1;
 	rol r24
 	sts patternState + 1, r24
-	;if there is no carry we want to just print out LED state
+	;if there is a carry the 7th bit (aka last bit) will be missing so we 
+	;manually set the bit that carries over upon the lsl and set it in the last bit for the 
+	;high bits
+	brcs fixPattern
+	;otherwise just print the pattern
+	jmp printPattern
+	
+printPattern:
+;load the pattern into the leds
+	lds leds, patternState
+;output the pattern to portC as required
+	out PORTC, leds
+;clear the temp counter as a second has passed
+	clear tempCounter
+;we want to now update our seconds counter
+;load into the register pair r24:r25 the secondCounter
+	lds r24, secondCounter
+	lds r25, secondCounter + 1
+;add 1 to the counter
+	adiw r25:r24, 1
+;to avoid any overwriting issues 
+	clear secondCounter
+;store the updated counter back into memory
+	sts secondCounter, r24
+	sts secondCounter + 1, r25
+;jump to end
+	rjmp endIF
+
+
+fixPattern:	
+	lds r24, patternState
+	;load 1 into r16
+	ldi r16, 1
+	;set the 0th bit aka 1 into the T flag as 1 -> 0000 0001<----- 0th bit 
+	bst r16, 0
+	;now we want to load into the 7th bit of our rotated byte the T flag aka 1
+	bld r24, 0
+	;store the updated byte
+	sts patternState, r24
 	jmp printPattern
 
-printPattern:
-	lds leds, patternState
-	out PORTC, leds
-
-
-	sts secondCounter, r24
-	sts secondCounter+1, r25
-	rjmp endIF
 
 NotSecond:
 	sts TempCounter, r24		;update the value of the temporary counter
 	sts TempCounter+1, r25	
+	jmp EndIf
 
 EndIf:
 	;epilogue
@@ -119,9 +153,9 @@ EndIf:
 
 main:
 ;we want to load our pattern into the data memory 
-	ldi r16, low(pattern)
+	ldi r16, high(pattern)
 	sts patternState, r16
-	ldi r17, high(pattern)
+	ldi r16, low(pattern)
 	sts patternState + 1, r16
 
 	lds leds, patternState
@@ -135,10 +169,7 @@ main:
 	out TCCR0B, temp				; prescalining = 8
 	ldi temp, 1<<TOIE0				; 128 microseconds
 	sts TIMSK0, temp				; T/C0 interrupt enable
-	sei								;enable gloable interrupt
-	
-loop: rjmp loop ; loop till mainual stop
+	sei
 
 
-
-
+loop: rjmp loop ; loop 
