0e5e77ba441f6e4a5192a17b58be8301741a2326
Abanob Tawfik <32289746+mariovsluigi@users.noreply.github.com>
2018-05-15 21:26:23 +1000

    helper functions need more

--- elevator/elevator/macros.asm
+++ elevator/elevator/macros.asm
@@ -1,9 +1,9 @@
-/*
- * macros.asm
- *
- *  Created: 15/05/2018 4:20:49 PM
- *   Author: Abs Tawfik
- */ 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//																																						 //
+//													helpful macros																						 //
+//																																						 //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 .macro prologue
 	in r16, SREG
@@ -52,4 +52,61 @@
 	st Y+, temp		;we clear the two bytes located where the label @a0 is by loading in a clear byte
 	st Y, temp
 
+.endmacro
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//																																						 //
+//													elevator queue macros																				 //
+//																																						 //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//a full queue would look like
+//full queue with floors 0-9 
+//0b (high bits) 0000011 (low bits) 11111111
+
+;this macro converts level into bits eg if floor is 6
+//converts 6 into 0b 00000000 01000000
+.macro CONVERT_FLOOR_INTEGER
+	prologue
+	lds temp, @0
+	;add 1 to temp since floor 0 is 0b0000000000000001
+	inc temp
+	loop:
+		cpi temp, 0
+		brlo endloop
+		dec temp
+		lsl @1
+		rol @2
+		jmp loop
+
+    endloop:
+		epilogue
+.endmacro
+
+; passing in 2 16 bit register pairs
+; 1 register pair is the current queue for the levels
+; the other register pair is the floor number we are adding
+.macro UPDATE_STATE_ADD
+	prologue
+	; or the low bits with low bits 
+	or @0, @1
+	; or the high bits with the high bits
+	or @2, @3
+	; clear the level bits
+	epilogue
+.endmacro
+
+; passing in 2 16 bit register pairs
+; 1 register pair is the current queue for the levels
+; the other register pair is the floor number we are removing
+.macro UPDATE_STATE_REMOVE
+	prologue
+	; exclusive or the low bits with the low bits
+	eor @0, @1
+	; exclusive or the high bits with the high bits
+	eor @2, @3
+	; clear the level bits
+	clear @0:@2
+	epilogue
 .endmacro
